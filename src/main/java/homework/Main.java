c9e1677aab762ebad08214343ae0d13c8cd90dff
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-03-26 06:52:05 +0300

    Готово

--- src/main/java/homework/Main.java
+++ src/main/java/homework/Main.java
@@ -3,12 +3,24 @@ package homework;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         Configuration configuration = new Configuration().configure();
         try (SessionFactory sessionFactory = configuration.buildSessionFactory()) {
             insertStudents(sessionFactory);
+            show(sessionFactory);
+            System.out.println("#############################");
+            getByAge(sessionFactory);
+            System.out.println("#############################");
+            updateStudents(sessionFactory, 2L);
+            show(sessionFactory);
+            System.out.println("#############################");
+            removeStudents(sessionFactory, 3L);
+            show(sessionFactory);
         }
     }
 
@@ -37,9 +49,43 @@ public class Main {
 
     public static void show(SessionFactory factory) {
         try (Session session = factory.openSession()) {
-            List<Student> list = session.
+            Query<Student> query = session.createQuery("select s from Student s", Student.class);
+            List<Student> resultList = query.getResultList();
+            resultList.forEach(System.out::println);
         }
     }
+
+    public static void getByAge(SessionFactory factory) {
+        try (Session session = factory.openSession()) {
+            Query<Student> query = session.createQuery("select s from Student s where age >= :age", Student.class);
+            query.setParameter("age", 20);
+            List<Student> resultList = query.getResultList();
+            resultList.forEach(System.out::println);
+        }
+    }
+
+    public static void updateStudents(SessionFactory factory, Long id) {
+        try (Session session = factory.openSession()) {
+            session.beginTransaction();
+            Student student = session.find(Student.class, id);
+            student.setFirstName("UPDATED");
+            session.merge(student);
+            session.getTransaction().commit();
+        }
+    }
+
+    public static void removeStudents(SessionFactory factory, Long id) {
+        try (Session session = factory.openSession()) {
+            session.beginTransaction();
+            Student student = session.find(Student.class, id);
+            session.remove(student);
+            session.getTransaction().commit();
+        }
+    }
+
+
+
+
 
 
 }
